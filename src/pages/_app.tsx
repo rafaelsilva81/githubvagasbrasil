c5035a7bec6a26692487b358abbe746961f6da6a
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-02-25 19:44:04 -0300

    style: finishing home page layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </QueryClientProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,63 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
+import RepoCard from "@/components/RepoCard";
+import lastAccessedAtom from "@/utils/lastAccessed";
+import repositoryList from "@/utils/repositoryList";
+import { useAtom } from "jotai";
+
 export default function Home() {
+  const porCategoria = repositoryList.filter(
+    (repo) => repo.type === "categoria"
+  );
+  const porTecnologia = repositoryList.filter(
+    (repo) => repo.type === "tecnologia"
+  );
+
+  const [lastAccessed] = useAtom(lastAccessedAtom);
+
+  const historico = repositoryList.filter((repo) =>
+    lastAccessed.includes(repo.slug)
+  );
+
   return (
     <>
       <Header />
 
-      <main></main>
+      <main className="flex flex-col gap-10 p-10">
+        {lastAccessed.length > 0 && (
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl font-semibold text-white">
+              Acessado recentemente
+            </h1>
+
+            <div className="flex flex-wrap gap-4">
+              {historico.map((repo, i) => (
+                <RepoCard key={i} repository={repo} />
+              ))}
+            </div>
+          </div>
+        )}
+        <div className="flex flex-col gap-4">
+          <h1 className="text-3xl font-semibold text-white">Por categoria</h1>
+
+          <div className="flex flex-wrap gap-4">
+            {porCategoria.map((repo, i) => (
+              <RepoCard key={i} repository={repo} />
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <h1 className="text-3xl font-semibold text-white">Por tecnologia</h1>
+
+          <div className="flex flex-wrap gap-4">
+            {porTecnologia.map((repo, i) => (
+              <RepoCard key={i} repository={repo} />
+            ))}
+          </div>
+        </div>
+      </main>
 
       <Footer />
     </>
