fd781f646eb35b0cc0564cd33fa4b6771e92e496
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-02-25 19:42:46 -0300

    feat: dynamic icon component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,16 +1,33 @@
-import React from "react";
-import * as Icons from "react-icons/all";
+import dynamic from "next/dynamic";
+import { IconBaseProps } from "react-icons/lib";
+
+interface typesPropsIcon {
+  nameIcon: string;
+  propsIcon?: IconBaseProps;
+}
 
 /* 
-  Esse componente é responsável por renderizar os ícones do react-icons
-  de acordo com o nome passado como propriedade.
-  
-  Exemplo de uso:
-  <Icon name="BiCodeAlt" className="text-2xl text-red-500" />
-*/
-const Icon = ({ name, className }: { name: string; className?: string }) => {
-  const SelectedIcon = Icons[name as keyof typeof Icons] || Icons.BiCodeAlt;
-  return <SelectedIcon className={className} />;
-};
+  Essa não é uma solução ideal, mas é a forma como encontrei para importar icones
+  dinamicamente, sem precisar instalar cada um deles separadamente.
 
-export default Icon;
+  Também é possível adicionar os ícones em uma pasta e importar de lá, mas
+  acredito que essa solução seja mais interessante caso hajam futuras atualizações
+  e apesar de tudo, esse componente não está pesando muito no bundle final.
+*/
+
+export function Icon({ nameIcon, propsIcon }: typesPropsIcon): JSX.Element {
+  const lib = nameIcon
+    .replace(/([a-z0-9])([A-Z])/g, "$1 $2")
+    .split(" ")[0]
+    .toLocaleLowerCase();
+
+  const ElementIcon = dynamic(
+    () =>
+      import(`react-icons/${lib}/index.js`).then((module) => module[nameIcon]),
+    {
+      ssr: false,
+    }
+  );
+
+  return <ElementIcon {...propsIcon} />;
+}
