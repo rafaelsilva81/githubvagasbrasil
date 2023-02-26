f5bc16558943bdf14edb56aac23cfce728e0a4f3
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-02-26 10:43:36 -0300

    feat: add issue type definition

---
+++ src/types/issues.d.ts
@@ -0,0 +1,57 @@
+type User = {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+};
+
+type Label = {
+  id: number;
+  node_id: string;
+  url: string;
+  name: string;
+  color: string;
+  default: boolean;
+  description: string | null;
+};
+
+type Issue = {
+  url: string;
+  repository_url: string;
+  labels_url: string;
+  comments_url: string;
+  events_url: string;
+  html_url: string;
+  id: number;
+  node_id: string;
+  number: number;
+  title: string;
+  user: User;
+  labels: Label[];
+  state: "open" | "closed";
+  locked: boolean;
+  assignee: User | null;
+  assignees: User[];
+  milestone: any;
+  comments: number;
+  created_at: string;
+  updated_at: string;
+  closed_at: string | null;
+  author_association: string;
+  active_lock_reason: string | null;
+  body: string;
+};
