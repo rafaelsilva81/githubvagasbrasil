4003fa8f9db9d552868f39f834bc4795fbb7f93a
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-02-26 11:10:05 -0300

    feat: add readme and about page

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,21 +1,39 @@
-# Github Vagas Brasil <img src="src/assets/logo-final.png" width="40" height="40"/>
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import React from "react";
+import ReactMarkdown from "react-markdown";
 
-## Sobre o projeto
+const markdown = `## Sobre o projeto
 
 Esse é um projeto open source que visa ajudar desenvolvedores brasileiros a encontrarem vagas divulgadas pelo github mais facilmente. Esse projeto foi desenvolvido por [@rafaelsilva81](https://github.com/rafaelsilva81)
 
-## Como funciona?
+### Como funciona?
 
 O projeto utiliza a [API do Github](https://api.github.com) para buscar issues abertas em diversos repositórios de vagas brasileiras e organiza essas vagas em uma página web separando por categoria (front-end, back-end, devops, etc) ou por tecnologia (react, go, python, etc).
 
-## Como contribuir
+### Como contribuir
 
 Eu pretendo adicionar novas funcionalidades e melhorar o projeto, mas também aceito contribuições de outras pessoas. Se você quiser contribuir, basta abrir uma issue ou um pull request.
 
 Caso você conheça algum repositório brasileiro de vagas que não está incluido no projeto ainda, basta abrir uma issue com o link do repositório ou entrar em contato comigo.
 
-## Tecnologias utilizadas
+### Tecnologias utilizadas
+NextJS, TailwindCSS, ReactQuery
+`;
 
-![Next JS](https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white)
-![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)
-![React Query](https://img.shields.io/badge/-React%20Query-FF4154?style=for-the-badge&logo=react%20query&logoColor=white)
+const Sobre = () => {
+  return (
+    <>
+      <Header />
+
+      <main className="flex flex-col w-full p-8">
+        <article className="flex flex-col min-w-full p-8 prose prose-md border prose-invert border-primary rounded-md">
+          <ReactMarkdown className="text-white">{markdown}</ReactMarkdown>
+        </article>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Sobre;
